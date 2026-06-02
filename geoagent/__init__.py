c073ff4e65a4534e04e038263c8dddad4ae6c06c
giswqs <giswqs@gmail.com>
2026-06-02 14:48:57 -0400

    chore: bump version to 1.8.2
    Patch release raising the package and QGIS plugin version metadata to 1.8.2.

--- geoagent/__init__.py
+++ geoagent/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Qiusheng Wu"""
 __email__ = "giswqs@gmail.com"
-__version__ = "1.8.1"
+__version__ = "1.8.2"
 
 from geoagent.core.config import GeoAgentConfig
 from geoagent.core.context import GeoAgentContext
